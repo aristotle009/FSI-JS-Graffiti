f3d263d939f7201d9b81e1e9d8358c471155ed8b
artis93 <105438768+artis93@users.noreply.github.com>
2022-07-21 20:43:57 -0700

    cahnges

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Select some elements...
+
+
+let header = document.querySelector('#page-header')
+header.style.textAlign = "right"
+document.body.style.backgroundColor = "red";
+
+let dogImages = document.querySelectorAll('.dog-image')
+
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = '100px'
+}
+
+
+
